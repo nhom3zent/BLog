53b771b515661a78224e1517b1ab2514ee3e872b
hoa <hoa2112@example.com>
2017-10-14 16:10:17 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,15 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('users', 'UserController@index');
 
+
+Route::get('blogs/create','BlogController@create')->name('blogs.create');
+
+Route::post('blogs/store','BlogController@store')->name('blogs.store');
+
+Route::get('blog/{blog_id}/edit','BlogController@edit')->name('blogs.edit');
+
+Route::put('blog/{blog_id}','BlogController@update')->name('blogs.update');
+
 Route::get('blogs/{id}','BlogController@detail')->name('blogs.detail');
+
+Route::delete('blogs/{id}','BlogController@destroy')->name('blogs.destroy');
